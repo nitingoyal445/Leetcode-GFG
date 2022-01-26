d44b7b46e15b80d17e07c463976ba118afb18407
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-01-26 23:23:52 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (27.63%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,20 +1,36 @@
 class Solution {
     public int climbStairs(int n) {
-        return climbMemo(n, new int[n+1]);
+        // return climbMemo(n, new int[n+1]);
+        return climbTabu(n);
     }
-    public int climbMemo(int n, int[] qb){
-        if(n==0){
-            return 1;
+    
+    public static int climbTabu(int n){
+        int[] ans = new int[n+1];
+        
+        ans[0] = 1;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                ans[i] = ans[i-1];
+            }else{
+                ans[i] = ans[i-1]+ans[i-2];
+            }
         }
-        else if(n<0){
-            return 0;
-        }
-        if(qb[n]!=0){
-            return qb[n];
-        }
-        int a = climbMemo(n-1, qb);
-        int b = climbMemo(n-2, qb);
-        qb[n] = a+b;
-        return a+b;
+        return ans[n];
     }
+    // public int climbMemo(int n, int[] qb){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     else if(n<0){
+    //         return 0;
+    //     }
+    //     if(qb[n]!=0){
+    //         return qb[n];
+    //     }
+    //     int a = climbMemo(n-1, qb);
+    //     int b = climbMemo(n-2, qb);
+    //     qb[n] = a+b;
+    //     return a+b;
+    // }
+    
 }
