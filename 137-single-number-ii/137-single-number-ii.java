20b94dd0cf31b814e8b48819eb3275023f6e9875
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-01-28 21:06:45 +0530

    Time: 4 ms (52.79%), Space: 43.7 MB (5.24%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    //approach - 1
     public int singleNumber(int[] nums) {
         int res = 0;
         for(int i=0;i<32;i++){
@@ -15,4 +16,27 @@ class Solution {
         }
         return res;
     }
+    
+    //approach - 2  //works for positive number
+//     public int singleNumber(int[] arr){
+//        int tn = Integer.MAX_VALUE; // 3n
+//        int tnp1 = 0; // 3n+1
+//        int tnp2 = 0; // 3n+2
+
+//        for(int i=0;i<arr.length;i++){
+//            int cwtn = arr[i] & tn;
+//            int cwtnp1 = arr[i] & tnp1;
+//            int cwtnp2 = arr[i] & tnp2;
+
+//            tn = tn & (~cwtn);
+//            tnp1 = tnp1 | cwtn;
+
+//            tnp1 = tnp1 & (~cwtnp1);
+//            tnp2 = tnp2 | cwtnp1;
+
+//            tnp2 = tnp2 & (~cwtnp2);
+//            tn = tn | cwtnp2;   
+//         }
+//         return tnp1;
+//     }
 }
