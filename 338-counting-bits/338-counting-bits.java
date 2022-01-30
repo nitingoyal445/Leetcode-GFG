d11606c02ff073080c477713b80974796cdf347e
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-01-30 21:31:28 +0530

    Time: 3 ms (53.81%), Space: 47.9 MB (6.67%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        
+        int[] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            arr[i] = countOnes(i);
+        }
+        return arr;
+    }
+    
+    private int countOnes(int val){
+        int c = 0;
+        while(val!=0){
+            int mask = val & (-val);
+            val = val-mask;
+            c++;
+        }
+        return c;
+    }
+}
