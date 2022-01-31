5edccbbdbcfe58dee914c168dfa298e387c3f33e
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-02-01 00:24:37 +0530

    Time: 3 ms (50.73%), Space: 46.2 MB (5.06%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -17,15 +17,6 @@ class Solution {
                 dp[i][j] = grid[i][j]+min;
             }
         }
-        
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-        }
-        
         return dp[0][0];
-        
     }
 }
