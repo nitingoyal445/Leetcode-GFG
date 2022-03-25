4ad19ea964d71fa5d79e3b51f0c495fcb4509498
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-03-25 23:18:12 +0530

    Time: 1 ms (74.84%), Space: 42.7 MB (73.21%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    // state 0 => camera is present
+    // state 1 => I am covered
+    // state 2 => I am unsafe
+    
+    static int camera = 0;
+    public int countCameraNeeded(TreeNode root){
+        if(root==null){
+            return 1; //null is safe , parent try to safe yourself
+        }
+        int leftState = countCameraNeeded(root.left);
+        int rightState = countCameraNeeded(root.right);
+        
+        //post area
+        if((leftState == 1) &&(rightState == 1)){
+            //root is unsafe
+            return 2;
+        }else if(leftState ==2 || rightState==2 ){
+            //camera is required
+            camera++;
+            return 0;
+        }else{
+            return 1;
+        }
+        
+    }
+    
+    public int minCameraCover(TreeNode root) {
+        camera = 0;
+        int finalState = countCameraNeeded(root);
+        if(finalState == 2){
+            camera++;
+        }
+        return camera;
+    }
+}
