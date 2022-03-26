3dadcfad5f1393682c6430449242faf4243ec198
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-03-26 12:30:04 +0530

    Time: 0 ms (100.00%), Space: 43 MB (83.39%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int binarySearch(int[] nums , int lo , int hi, int target){
+        if(lo>hi){
+            return -1;    
+        }
+        int mid = lo + (hi-lo)/2;
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid]<target){
+            return binarySearch(nums, mid+1, hi, target);
+        }else{
+            return binarySearch(nums, lo, mid-1, target);
+        }
+    }
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length-1, target);
+    }
+}
