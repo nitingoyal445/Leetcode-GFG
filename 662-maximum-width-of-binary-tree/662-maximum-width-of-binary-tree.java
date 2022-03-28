9ec2a532e028b92e0aa35ae93a761b5c1801365d
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-03-29 00:15:44 +0530

    Time: 2 ms (78.78%), Space: 44 MB (26.18%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -14,19 +14,33 @@
  * }
  */
 class Solution {
-     public int widthOfBinaryTree(TreeNode root) {
-        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
+    
+    class WPair{
+        TreeNode node;
+        int index;
+        
+        WPair(TreeNode node, int index){
+            this.node = node;
+            this.index = index;
+        }
     }
     
-    public int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end){
-        if(root == null)return 0;
-        if(start.size() == level){
-            start.add(order); end.add(order);
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<WPair> qu = new LinkedList<>();
+        qu.add(new WPair(root, 0));
+        int maxWidth = 0;
+        while(qu.size()>0){
+            int size = qu.size();
+            int lmi = qu.peek().index;  // left most index
+            int rmi = 0;              // right most index
+            while(size-->0){
+                WPair rem = qu.remove();
+                rmi = rem.index;
+                if(rem.node.left!=null) qu.add(new WPair(rem.node.left, 2*rem.index+1));
+                if(rem.node.right!=null) qu.add(new WPair(rem.node.right, 2*rem.index+2));
+                maxWidth = Math.max(maxWidth, rmi-lmi+1);
+            }
         }
-        else end.set(level, order);
-        int cur = end.get(level) - start.get(level) + 1;
-        int left = dfs(root.left, level + 1, 2*order, start, end);
-        int right = dfs(root.right, level + 1, 2*order + 1, start, end);
-        return Math.max(cur, Math.max(left, right));
+        return maxWidth;
     }
 }
