ceca5f2e025e42a3da0002d6f006cd70f2005d0c
Nitin Goyal <35740188+nitingoyal445@users.noreply.github.com>
2022-04-30 14:58:25 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (28.95%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -13,7 +13,7 @@ class Solution {
         
         ListNode prev = null;
         ListNode curr = head;
-        while(curr != null){
+        while(curr!=null){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
